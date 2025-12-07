c5ff19353948dd20910381b04440034a4fabbf0e
white333333 <107554036+white333333@users.noreply.github.com>
2025-12-07 15:21:28 +0100

    rework how main function works in main.py. its less cluttered now but not sure if good

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ def get_book_text(path):
     with open(path) as file:
         return file.read()
 
-def main():
+""" def main():
     text_result = get_book_text("books/frankenstein.txt")
-    print(text_result)
+    print(text_result) """
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
 
 main()
 
