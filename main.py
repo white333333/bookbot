fa320329310f0a7446aeeff40176622e68138104
white333333 <107554036+white333333@users.noreply.github.com>
2025-12-09 17:29:39 +0100

    improve presentation

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ book_text = main()
 counted_characters_final = count_characters(book_text)
 dictionary_list = list_maker(counted_characters_final)
 
-    
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {relative_path}")
+print("----------- Word Count ----------")
 print(f"Found {count_words(book_text)} total words")
+print("--------- Character Count -------")
 for item in dictionary_list:
     letter = item["char"]
     count = item["num"]
